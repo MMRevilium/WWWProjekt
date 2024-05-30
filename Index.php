2871c9b4462f98863412eec287f7ba7d57d05e20
MMRevilium <Mati6499@gmail.com>
2024-05-30 17:16:34 +0200

    Basic structure, beginning css

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AlbionBuilder</title>
+
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <header>
+  Test
+  </header>
+  <div id="main">
+
+  </div>
+  <footer>
+
+  </footer>
+</body>
+</html>

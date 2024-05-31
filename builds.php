63109a1459321311a52e9578e21d7e33f611c110
MMRevilium <Mati6499@gmail.com>
2024-05-31 12:11:02 +0200

    Add links, Style nav links, Add 3 pages

--- builds.php
+++ builds.php
@@ -3,19 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AlbionBuilder</title>
-
-  <link rel="stylesheet" href="css/main.css">
+  <title>Document</title>
 </head>
 <body>
-  <nav>
+<nav>
     <a href="Index.php"><div class="nav-option">Strona Główna</div></a>
     <a href="builds.php"><div class="nav-option">Lista buildów</div></a>
     <a href="creator.php"><div class="nav-option">Stwórz własny build</div></a>
     <a href="login.html"><div class="nav-option">Zaloguj się</div></a>
   </nav>
   <div id="main">
-
+    
   </div>
   <footer>
 
